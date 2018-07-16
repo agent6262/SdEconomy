2ef76bd173d9b0dbdf67b7e083b44c49db7657a2
Tyler Bucher <tylerb318@gmail.com>
2018-07-16 14:17:14 -0500

    Added chat colors and formatting.

--- src/spigot/java/net/reallifegames/sdeconomy/commands/TransactionCommand.java
+++ src/spigot/java/net/reallifegames/sdeconomy/commands/TransactionCommand.java
@@ -23,14 +23,15 @@
  */
 package net.reallifegames.sdeconomy.commands;
 
+import net.reallifegames.sdeconomy.Product;
 import net.reallifegames.sdeconomy.SdEconomy;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.sql.*;
+import java.util.Collection;
 import java.util.logging.Level;
 
 import static net.reallifegames.sdeconomy.SqlService.SEARCH_USERS_TRANSACTIONS;
@@ -63,14 +64,14 @@ public class TransactionCommand extends BaseCommand {
     @Override
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
         // Check for arg length
-        if (args.length != 2) {
-            sender.sendMessage(ChatColor.RED + "You need to specify the player uuid and page number.");
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "You need to specify the player uuid or and the page number.");
             return false;
         }
         // Get amount of an item
         final int pageNumber;
         try {
-            int parse = Integer.parseInt(args[1]) - 1;
+            int parse = args.length == 1 ? 0 : Integer.parseInt(args[1]) - 1;
             pageNumber = parse < 0 ? 0 : parse;
         } catch (NumberFormatException | NullPointerException e) {
             sender.sendMessage(ChatColor.RED + args[1] + " is not a number.");
@@ -85,20 +86,26 @@ public class TransactionCommand extends BaseCommand {
             searchStatement.setInt(2, pageNumber);
             // Execute query
             final ResultSet resultSet = searchStatement.executeQuery();
-            boolean returns = false;
-            while (resultSet.next()) {
-                returns = true;
-                sender.sendMessage(getTextAction(resultSet.getInt("action")) + " " +
-                        resultSet.getString("alias") + " " + resultSet.getString("date")
-                        + " " + resultSet.getFloat("amount"));
+            if (!resultSet.next()) {
+                sender.sendMessage("Player has no rows for this page number or has not interacted with the economy yet.");
+                return true;
             }
+            final StringBuilder builder = new StringBuilder();
+            final int aliasMaxLength = getMaxLength(pluginInstance.getStockPrices().values());
+            do {
+                builder.append(ChatColor.DARK_AQUA)
+                        .append(postPadString(getTextAction(resultSet.getInt("action")), 9)).append(' ')
+                        .append(ChatColor.RESET).append(postPadString(resultSet.getString("alias"), aliasMaxLength))
+                        .append(' ').append(ChatColor.GOLD)
+                        .append(postPadString(resultSet.getString("date"), 19)).append(' ')
+                        .append(ChatColor.GREEN).append(resultSet.getFloat("amount"));
+                sender.sendMessage(builder.toString());
+                builder.setLength(0);
+            } while (resultSet.next());
             // Close objects
             resultSet.close();
             searchStatement.close();
             sqlConnection.close();
-            if (!returns) {
-                sender.sendMessage("Player has no rows for this page number or has not interacted with the economy yet.");
-            }
             return true;
         } catch (SQLException e) {
             pluginInstance.getLogger().log(Level.SEVERE, "Unable to access database.", e);
@@ -111,6 +118,7 @@ public class TransactionCommand extends BaseCommand {
      * @param action the action to get text for.
      * @return the action or null if not found.
      */
+    @Nonnull
     private static String getTextAction(final int action) {
         switch (action) {
             case 0:
@@ -120,6 +128,36 @@ public class TransactionCommand extends BaseCommand {
             case 2:
                 return "SELL";
         }
-        return null;
+        return "";
+    }
+
+    /**
+     * @param text      the text to check and pad.
+     * @param maxLength the maximum length of the padded string to match.
+     * @return the newly padded string or the passed original.
+     */
+    private static String postPadString(@Nonnull final String text, final int maxLength) {
+        if (text.length() < maxLength) {
+            final StringBuilder builder = new StringBuilder(text);
+            for (int i = 0; i < maxLength - text.length(); i++) {
+                builder.append(' ');
+            }
+            return builder.toString();
+        }
+        return text;
+    }
+
+    /**
+     * @param productList the list of products to check
+     * @return the maximum length of an alias.
+     */
+    private static int getMaxLength(@Nonnull final Collection<Product> productList) {
+        int length = 0;
+        for (final Product product : productList) {
+            if (product.alias.length() > length) {
+                length = product.alias.length();
+            }
+        }
+        return length;
     }
 }
